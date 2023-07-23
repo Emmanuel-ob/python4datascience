d0c225f9aac2c751ff2be3ba2816a08ad427e614
Emmanuel Obute <emmanuel6.obute@gmail.com>
2023-07-23 15:59:02 +0100

    Python for data science 12 weeks

---
+++ week 1/Age_calculator.py
@@ -0,0 +1,5 @@
+birthYear = float(input("\nEnter your birth year, i.e 1990 "))
+currentYear = float(input("\nEnter current year, i.e 2017 "))
+age = currentYear - birthYear
+print("Your age is  " + str(age) + "  years")
+input("Hit enter button to exit.")
